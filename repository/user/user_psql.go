f1519fe4fda4544050a882cdce3f012faaebc192
Ajay Kumar <ajaykumar@Ajays-MacBook-Pro.local>
2019-04-21 09:43:38 +0530

    formatted the file

--- repository/user/user_psql.go
+++ repository/user/user_psql.go
@@ -2,7 +2,6 @@ package userRepository
 
 import (
 	"database/sql"
-	"fmt"
 	"log"
 	"movie-api/models"
 )
@@ -16,16 +15,10 @@ func logFatal(err error)  {
 }
 
 func (c UserRepository) Signup(db *sql.DB, user models.RegisteredUser) models.RegisteredUser {
-	stmt := "insert into users (email, password, userid) values ($1, $2, $3) returning id;"
-	err := db.QueryRow(stmt, user.Email, user.Password, user.UserId).Scan(&user.ID);
-	if err != nil {
-		if err == sql.ErrNoRows {
-			fmt.Println("Zero rows found")
-		}else {
-			fmt.Println("Else condition")
-			logFatal(err);
-		}
-	}
+	//stmt := "insert into users (email, password, userid) values ($1, $2, $3) returning id;"
+	var userId int
+	err := db.QueryRow("insert into users (email, password, userid) values ($1, $2, $3) returning id;", user.Email, user.Password, user.UserId).Scan(&userId);
+	logFatal(err)
 	user.Password = ""
 	return user
 }
