77fda35f524d7b37ef536dd30cf4809a5ba80794
ajayKr <ajay.k@teliax.com>
2019-04-23 16:15:22 +0530

    generated unique id for user

--- repository/user/user_psql.go
+++ repository/user/user_psql.go
@@ -2,9 +2,8 @@ package userRepository
 
 import (
 	"database/sql"
-	"fmt"
-	uuid "github.com/nu7hatch/gouuid"
 	"log"
+	"movie-api/helper"
 	"movie-api/models"
 )
 
@@ -17,12 +16,10 @@ func logFatal(err error) {
 }
 
 func (c UserRepository) Signup(db *sql.DB, user models.RegisteredUser) models.RegisteredUser {
-	u4, err := uuid.NewV4()
-	fmt.Println("u4: ", u4);
-	fmt.Printf("type of u4 is : %T\n", u4)
-	stmt := "insert into users (email, password, userid) values ($1, $2, $3) returning id;"
-	//hash, err := bcrypt.GenerateFromPassword([]byte(user.Password), 10)
-	err = db.QueryRow(stmt, user.Email, user.Password, user.UserId).Scan(&user.ID);
+	userId := helper.GenerateUserID()
+	stmt := "insert into users (email, password, userid) values ($1, $2, $3) returning id,userid"
+	row := db.QueryRow(stmt, user.Email, user.Password, userId)
+	err := row.Scan(&user.ID, &user.UserId)
 	logFatal(err)
 	user.Password = ""
 	return user
