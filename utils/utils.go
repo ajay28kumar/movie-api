5f10207bc31f9274c6b9d3690cffac27e723c331
Ajay Kumar <ajaykumar@Ajays-MacBook-Pro.local>
2019-04-28 13:40:34 +0530

    added verifymiddlewaretoken

--- utils/utils.go
+++ utils/utils.go
@@ -2,10 +2,12 @@ package utils
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/dgrijalva/jwt-go"
 	"movie-api/models"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func RespondWithError(w http.ResponseWriter, status int, error models.Error) {
@@ -43,4 +45,36 @@ func GenerateToken(user models.UserDetails) (string, error) {
 		return tokenString, err
 	}
 	return tokenString, nil
+}
+
+func TokenVerifyMiddleWare(next http.HandlerFunc) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		authHeader := r.Header.Get("Authorization")
+		bearerToken := strings.Split(authHeader," ")
+		if len(bearerToken) == 2 {
+			authToken := bearerToken[1]
+			token,error := jwt.Parse(authToken, func(token *jwt.Token) (interface{}, error) {
+				if _,ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+					return nil,fmt.Errorf("There was an error")
+				}
+				return []byte("secret"), nil
+			})
+			if error != nil {
+				//errorObject.Message=error.Error()
+				RespondWithError(w,http.StatusUnauthorized, models.Error{error.Error(),401, "UNAUTHARIZED" })
+				return
+			}
+			if token.Valid {
+				next.ServeHTTP(w,r)
+			}else{
+				RespondWithError(w,http.StatusUnauthorized, models.Error{error.Error(),401, "UNAUTHARIZED" })
+				return
+			}
+
+		}else {
+			RespondWithError(w,http.StatusUnauthorized, models.Error{"invalid token",401, "UNAUTHARIZED" })
+			return
+		}
+	})
 }
